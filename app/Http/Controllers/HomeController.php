a4ce40b936256a7b4ed1dc834ca126eeb9e29221
Sabrina Afrin <tomaju304@gmail.com>
2024-01-01 21:53:40 +0600

    New model added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Job\Job;
+
 
 class HomeController extends Controller
 {
@@ -23,6 +25,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $jobs = Job::select()->take(5)->orderby('id','desc')->get();
+        $totalJobs = Job::all()->count();
+
+
+        return view('home',copact('jobs','totalJobs'));
     }
 }
